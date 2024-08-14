b016e8bfb440d36c92c092956be6dbe131fe7a38
AswathiVV <aswathi.v.v.916@gmail.com>
2024-08-14 13:36:55 +0530

    python-op&str

--- operatives.py
+++ operatives.py
@@ -1,54 +1,111 @@
-print(10+5)
-print(10-5)
-print(10/5)
-print(10%3)
-print(10**2)
-print(10//3)
-print(10**2)
-print('python'*4)
-print('sreelaya' + 'prabu')
+# print(10+5)
+# print(10-5)
+# print(10/5)
+# print(10%3)
+# print(10**2)
+# print(10//3)
+# print(10**2)
+# print('python'*4)
+# print('sreelaya' + 'prabu')
 
-a=25
-a+=5
-print(a)
+# a=25
+# a+=5
+# print(a)
 
-a=25
-a-=5
-print(a)
+# a=25
+# a-=5
+# print(a)
 
-a=20
-a/=5
-print(a)
+# a=20
+# a/=5
+# print(a)
 
-a=20
-a%=7
-print(a)
+# a=20
+# a%=7
+# print(a)
 
-a=20
-a//=5
-print(a)
+# a=20
+# a//=5
+# print(a)
 
-a=10
-a**=2
-print(a)
+# a=10
+# a**=2
+# print(a)
 
-a=10
-a%=3
-print(a)
+# a=10
+# a%=3
+# print(a)
 
-a=20
-a*=2
-print(a)
+# a=20
+# a*=2
+# print(a)
 
-a='x'
-a*=3
-print(a)
+# a='x'
+# a*=3
+# print(a)
 
-print(10==10)
-print(10==15)
-print(10>=10)
-print(10<=15)
-print(10>=15)
-print(10!=10)
-print(10!=12)
-print(15<=10)
+# print(10==10)
+# print(10==15)
+# print(10>=10)
+# print(10<=15)
+# print(10>=15)
+# print(10!=10)
+# print(10!=12)
+# print(15<=10)
+
+# print(10==10 and 10>=11)
+# print(10==10 and 10>=9)
+# print(10==9 and 10<=9)
+
+# print(10==10 or 10>=11)
+# print(10==12 or 10<=8)
+
+# print(not(10<15))
+# print(not(10==10))
+# print(not(10==12))
+# print(not(10>15))
+
+
+# a="python"
+# b="t"
+# print(b in a)
+
+# a="[10,11,12]"
+# b="9"
+# print(b in a)
+
+# a="python"
+# b="t"
+# print(b not in a)
+
+# a=[10,11,12]
+# b=9
+# print(b not in a)
+
+l=[1,2,3]
+l1=[1,2,3]
+print(l==l1)
+print(id(l))
+print(id(l1))
+print(l is l1)
+
+l=[10,20,30,40]
+l1=[50,60,70]
+print(l==l1)
+print(id(l))
+print(id(l1))
+print(l is l1)
+
+a=[1,2,3]
+b=[1,2,3]
+print(a==b)
+print(id(a))
+print(id(b))
+print(a is b)
+
+a="achu"
+b="a"
+
+print(id(a))
+print(id(b))
+print(a is not b)
---
+++ string.py
@@ -0,0 +1,15 @@
+a='python'
+print('\tpython\n','\tjava')
+
+print(a[0])
+print(a[-1])
+print(a[-6])
+print(a[5])
+
+a='''python
+php'''
+print(a)
+
+a="""python
+php"""
+print(a)
