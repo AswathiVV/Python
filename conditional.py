ee7c66633386cded093df5b4754beda73b5c48c4
AswathiVV <aswathi.v.v.916@gmail.com>
2024-08-17 13:30:35 +0530

    Conditional

--- conditional.py
+++ conditional.py
@@ -19,8 +19,89 @@
 # else:
 #     print(b,"is the bigger no.")             
 
-a=int(input("Enter ano. : "))    
-if a%3==0:
-    print(a,"Divisible")
+# a=int(input("Enter ano. : "))    
+# if a%3==0:
+#     print(a,"Divisible")
+# else:
+#     print(a,"Not Divisible")    
+
+# a=int(input("Enter Your Salary")) 
+# b=int(input("Enter Your Year Of Experience :"))    
+# if b>=5 :
+#     bonus=0.05*a
+#     print(f"Net Bonus Amount:{bonus:.5}")
+# else:
+#     print("You are not eligible for bonus")    
+
+# a=int(input("Enter 1st no.:"))
+# b=int(input("Enter 2nd no.:"))
+
+# if a==b:
+#     print("eql")
+# elif a>b:
+#     print(a)
+# else:
+#     print(b)    
+
+# a=int(input("Enter a no."))
+# if a==1:
+#     print("Sunday")
+# elif a==2:
+#     print("Monday")
+# elif a==3:
+#     print("Tuesday")        
+# elif a==4:
+#     print("Wednessday")
+# elif a==3:
+#     print("Thursday")   
+# elif a==3:
+#     print("Friday")                  
+# elif a==3:
+#     print("Saturday")       
+# else:
+#     print("Invalid no.")    
+
+# a=input("Enter Your City : ")
+# if a=="Delhi":
+#     print("Red Fort")
+# elif a=="Agra":
+#     print("Taj Mahal")
+# elif a=="Jaipur":
+#     print("Jal Mahal")    
+
+# a=int(input("Cost Price of Bike :"))
+# if a>100000:
+#     Tax=a*.15
+#     print(f"Road Tax :{Tax}")
+# elif a<=100000 and a>=50000:
+#     Tax=a*.10
+#     print(f"Road Tax :{Tax}")
+# else:
+#     Tax=a*.05
+#     print(f"Road Tax :{Tax}")    
+
+# units=int(input("No. of units consumed :"))
+# if units<100:
+#     print("No Charge")
+# elif units>100 and units<200:
+#     fee=5*(units-100)
+#     print(f"Fee:{fee}")   
+# else:
+#     fee=((units-200)*10)+500
+#     print(f"Fee:{fee}")     
+
+a=int(input("Enter 1st no. :")) 
+b=int(input("Enter 2nd no. :"))
+c=int(input("Enter 3rd no. :"))
+if a>b:
+     if a>c:
+          print(a)
+     else:
+          print(c)     
 else:
-    print(a,"Not Divisible")    
+     if b>c:
+          print(b)
+     else:
+          print(c)     
+
+ 
