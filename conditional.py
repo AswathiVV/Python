db3cf98521b745dc15f8d06f98267733f1e0ebfd
AswathiVV <aswathi.v.v.916@gmail.com>
2024-08-19 13:53:53 +0530

    looping

--- conditional.py
+++ conditional.py
@@ -90,18 +90,18 @@
 #     fee=((units-200)*10)+500
 #     print(f"Fee:{fee}")     
 
-a=int(input("Enter 1st no. :")) 
-b=int(input("Enter 2nd no. :"))
-c=int(input("Enter 3rd no. :"))
-if a>b:
-     if a>c:
-          print(a)
-     else:
-          print(c)     
-else:
-     if b>c:
-          print(b)
-     else:
-          print(c)     
+# a=int(input("Enter 1st no. :")) 
+# b=int(input("Enter 2nd no. :"))
+# c=int(input("Enter 3rd no. :"))
+# if a>b:
+#      if a>c:
+#           print(a)
+#      else:
+#           print(c)     
+# else:
+#      if b>c:
+#           print(b)
+#      else:
+#           print(c)     
 
  
---
+++ looping_stm.py
@@ -0,0 +1,92 @@
+# i=int(input("Enter Starting Value :"))
+# e=int(input("Enter Ending Value :"))
+# while i<=e:
+#     print(i)
+#     i+=1
+
+# i=int(input("Enter Starting Value :"))
+# e=int(input("Enter Ending Value :"))
+# while i<=e:
+#     print(i)
+#     i+=5
+
+# i=int(input("Enter Starting Value :"))
+# e=int(input("Enter Ending Value :"))
+# while i<=e:
+#     print(i)
+#     i+=10
+
+# i=1
+# e=int(input("Enter limit :"))
+# while i<=e:
+#     print("python")
+#     i+=1
+
+# i=int(input("Enter Starting Value :"))
+# e=int(input("Enter Ending Value :"))
+# sum=0
+# while i<=e:
+#     sum=sum+i
+#     i+=1
+# print('sum:',sum)    
+
+# i=int(input("Enter Starting Value :"))
+# e=int(input("Enter Ending Value :"))
+# while i<=e:
+#     if i%2==0:
+#         print(i) 
+#     i+=1
+
+# i=int(input("Enter Starting Value :"))
+# e=int(input("Enter Ending Value :"))
+# sum=0
+# while i<=e:
+#     if i%3==0:
+#         sum=+i
+#     i+=1
+# print('sum: ',sum) 
+
+
+# i=int(input("Enter Starting Value :"))
+# e=int(input("Enter Ending Value :"))
+# sum=0
+# while i<=e:
+#     if i%2==0:
+#         sum+=i
+#     i+=1
+# print('sum: ',sum) 
+
+# i=int(input("Enter Starting Value :"))
+# e=int(input("Enter Ending Value :"))
+# sum=0
+# while i<=e:
+#     if i%2!=0:
+#         sum+=i
+#     i+=1
+# print('sum: ',sum) 
+
+
+# x=int(input("Enter Starting Value :"))
+# y=int(input("Enter Ending Value :"))
+# i=1
+# sum=0
+# e=int(input("Enter a no. :"))
+# while i<=10 and x<=y:
+#     print(i,'*',e,'=',i*e and 'sum: ',sum)
+#     i+=1
+#     x+=1
+
+
+i=int(input("Enter Starting Value :" ,"Enter Starting Value :"))
+e=int(input("Enter Ending Value :" ,"Enter Ending Value :"))
+sum=0
+while i<=e and i<=e:
+    if i%2==0 and i%2!=0:
+        sum+=i and sum+i
+    i+=1 and i+1
+print('sum: ',sum) and ('sum: ',sum)
+
+
+
+
+     
