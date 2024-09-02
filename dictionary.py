a53f3b32b27979b690ffb8030f90ecfda54e606c
AswathiVV <aswathi.v.v.916@gmail.com>
2024-09-02 13:29:37 +0530

    Dict

---
+++ dictionary.py
@@ -0,0 +1,59 @@
+d={'name':'anu','age':20,'mark':20}
+# print(d['name'])
+# d['name']='Aswathi'
+# print(d)
+# d['place']='EKM'
+# print(d)
+
+# for i in d:
+#     print(i,d[i])
+'''name anu
+   age 20
+   mark 20'''
+
+# for i in d:
+#     print(i)
+'''name
+   age
+   mark'''
+
+# for i in d:
+#     print (d[i])
+'''anu
+   20
+   20'''
+
+# if d['name']=='anu':
+#     print('True')
+# else:
+#     print('False')  
+# '''True'''  
+
+# print(d.get ('name'))
+'''anu'''  
+
+# d.update({'name':'Akhil'})
+# print(d)
+'''{'name': 'Akhil', 'age': 20, 'mark': 20}'''
+
+# d.pop('name')
+# print(d)
+'''{'age': 20, 'mark': 20}'''
+
+# d.popitem()
+# print(d)
+'''{'name': 'anu', 'age': 20}''' 
+
+# print(d.get('place'))
+'''None'''
+
+# d={}
+# l=['name','age','place','mark']
+# d=d.fromkeys(l)
+# print(d)
+'''{'name': None, 'age': None, 'place': None, 'mark': None}'''
+
+# d={}
+# d.setdefault('name')
+# print(d)
+'''{'name': None}'''
