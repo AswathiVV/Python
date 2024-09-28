415627f3d4e6cfadf41c40af835b79461f9f42a1
AswathiVV <aswathi.v.v.916@gmail.com>
2024-09-28 13:35:38 +0530

    inheritance

--- OOP.py
+++ OOP.py
@@ -67,3 +67,185 @@ display : 500
 # dtls(age=20,name='a')        
 
 # obj=Demo(12)  
+
+
+
+
+# single inheritance
+
+
+# class parent:
+#     def shop(self):
+#         print("shop")
+#     def car(self):
+#         print("car")
+# class child(parent):
+#     def bike(self):
+#         print("bike")
+
+# manu=child()
+# manu.bike()
+# manu.car()
+# manu.shop()
+
+
+# class synnefo:
+#     def php(self):
+#         print("php")
+#     def python(self):
+#         print("python")
+
+# class novavi(synnefo):
+#     def web_dev(self):
+#         print("web development")  
+#     def dm(self):
+#         print("dm works")
+
+# manu=novavi()
+# manu.python()
+# manu.php()
+# manu.web_dev()
+                    
+# std1=synnefo()
+# std1.php()  
+
+
+
+# multiple inheritance
+
+
+# class mom:
+#     def shares(self):
+#         print("shares")
+#     def land(self):
+#         print("land") 
+# class dad:
+#     def investments(self):
+#         print("investments")
+#     def assets(self):
+#         print("assets")
+# class child(mom,dad):
+#     def bike(self):
+#         print("bike")
+
+# riya=child()
+# riya.assets()
+# riya.shares()
+# riya.bike()
+
+
+# class school:
+#     def reports(self):
+#         print('reports')
+#     def lab(self):
+#         print('lab')
+# class library:
+#     def books(self):
+#         print('books')
+#     def lib_room(self):
+#         print('library room') 
+# class std1(school,library):
+#     def id_card(self):
+#         print('id card')                       
+
+# sana=std1()
+# sana.lab()
+# sana.books()
+# sana.id_card()
+
+
+
+# multilevel inheritance
+
+
+# class grand_father:
+#     def shares(self):
+#         print("shares")
+#     def land(self):
+#         print("land") 
+
+# class father(grand_father):
+#     def investments(self):
+#         print("investments")
+#     def assets(self):
+#         print("assets")
+
+# class child(father):
+#     def bike(self):
+#         print("bike")
+
+# anu=child()
+# anu.shares()
+# anu.assets()
+# anu.bike()
+
+# manu=father()
+# manu.land()
+# manu.investments()
+
+
+# class dep:
+#     def employees(self):
+#         print('employee details')
+#     def works(self):  
+#         print('work details')   
+# class dep_managers(dep):
+#     def reports(self):
+#         print('reports')
+# class dep_of_mgt(dep_managers):
+#     def final_reports(self):
+#         print('final reports')    
+
+# mg=dep_of_mgt()
+# mg.employees()
+# mg.reports()
+# mg.final_reports()     
+
+
+# higherarcial inheritance
+
+# class mom:
+#      def shares(self):
+#         print("shares")
+#      def assets(self):
+#         print("assets")
+
+# class child1(mom):
+#     def car(self):
+#         print('car')     
+
+# class child2(mom):
+#     def bike(self):
+#         print('bike')
+
+# manu=child1()
+# manu.assets()
+# manu.shares()
+# manu.car()   
+
+# anu=child2()
+# anu.assets()
+# anu.shares()
+# anu.bike()
+
+
+class BOD:
+    def final_reports(self):
+        print('final reports')
+    def plans_and_policies(self):
+        print('plans & policies')
+class production_dep(BOD):
+    def employees(self):
+        print('emp')    
+class sales_dep(BOD) :
+    def employees(self):
+        print('emp') 
+
+
+p=production_dep()
+p.final_reports()
+
+s=sales_dep()
+p.final_reports
+
+   
