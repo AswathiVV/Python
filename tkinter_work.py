390046f7d3724dcff85a37b8be1522b2e0916fc5
AswathiVV <aswathi.v.v.916@gmail.com>
2024-10-16 10:39:35 +0530

    Login_&_Register

--- tkinter_work.py
+++ tkinter_work.py
@@ -1,5 +1,5 @@
-import sqlite3
 import tkinter
+import sqlite3
 
 # win=tkinter.Tk()
 # win.title("tkinter intro")
@@ -34,6 +34,9 @@ win.maxsize(500,500)
 win.minsize(400,400)
 win.configure(bg="lightblue")
 
+    
+    
+
 def reg_form():
     win1=tkinter.Tk()
     win1.title("Register Page")
@@ -41,55 +44,53 @@ def reg_form():
     win1.minsize(400,400)
     win1.configure(bg="lightblue")
     def reg():
-        con=sqlite3.connect("Python/sample1.db")    
-        con.execute("create table user(Username text,Password text)") 
-        con.execute("insert into user(Username,Password)values(?,?)",(e1.get(),e2.get()))
+        con=sqlite3.connect("python/tkinter_work.db")
+        # con.execute("create table user(uname text,password text)")
+        con.execute("insert into user(uname,password)values(?,?)",(e1.get(),e2.get()))
         con.commit()
         win1.destroy()
 
 
- 
-
-    l1=tkinter.Label(win,text="Register form",bg="lightblue",fg="black")
+    l1=tkinter.Label(win1,text="Register form",bg="lightblue",fg="black")
     l1.place(x=180,y=10)
 
-    l2=tkinter.Label(win,text="Username",bg="lightblue",fg="black")
+
+    l2=tkinter.Label(win1,text="Username",bg="lightblue",fg="black")
     l2.place(x=80,y=40)
-    e1=tkinter.Entry(win)
+    e1=tkinter.Entry(win1)
     e1.place(x=200,y=40)
 
-    l3=tkinter.Label(win,text="Password",bg="lightblue",fg="black")
+    l3=tkinter.Label(win1,text="Password",bg="lightblue",fg="black")
     l3.place(x=80,y=70)
-    e2=tkinter.Entry(win)
+    e2=tkinter.Entry(win1)
     e2.place(x=200,y=70)
 
-    b1=tkinter.Button(win,text="Register",bg="black",activebackground="gray",fg="white",activeforeground='green',padx=10,pady=10,command=reg)
+    b1=tkinter.Button(win1,text="Register",bg="black",activebackground="gray",fg="white",activeforeground='green',padx=10,pady=10,command=reg)
     b1.place(x=150,y=120)
-
     win1.mainloop()
-
-def home():
+def Home():
     win2=tkinter.Tk()
-    l1=tkinter.Label(win2,text="HOME PAGE")
+    l1=tkinter.Label(win2,text='Home page')
     l1.pack()
     b1=tkinter.Button(win2,text="logout",command=win2.quit)
     b1.pack()
     win2.mainloop()
 
 def login():
-    con=sqlite3.connect("Python/sample1.db")
-    data=con.execute("select * from user where Username=? and Password=?",(e1.get(),e2.get()))
+    con=sqlite3.connect("python/tkinter_work.db")
+    data=con.execute("select * from user where uname=? and password=?",(e1.get(),e2.get()))
     f=0
     for i in data:
         f=1
-        home()
+        Home()
     if f==0:
-        l4.config(text="invalid username or password")   
+        l4.config(text="invalid username or password.")    
+
 
 l1=tkinter.Label(win,text="Login page",bg="lightblue",fg="black")
 l1.place(x=170,y=10)
 l4=tkinter.Label(win)
-l4.place(x=150,y=30)    
+l4.place(x=80,y=40)
 l2=tkinter.Label(win,text="Username",bg="lightblue",fg="black")
 l2.place(x=80,y=40)
 e1=tkinter.Entry(win)
@@ -100,7 +101,7 @@ l3.place(x=80,y=70)
 e2=tkinter.Entry(win)
 e2.place(x=200,y=70)
 
-b1=tkinter.Button(win,text="login",bg="white",activebackground="gray",fg="black",activeforeground='green',padx=10,pady=10,command=login)
+b1=tkinter.Button(win,text="Submit",bg="white",activebackground="gray",fg="black",activeforeground='green',padx=10,pady=10,command=login)
 b1.place(x=150,y=120)
 
 b2=tkinter.Button(win,text="Register",bg="black",activebackground="gray",fg="white",activeforeground='green',padx=10,pady=10,command=reg_form)
